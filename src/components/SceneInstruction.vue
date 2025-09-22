5c0f738d60b8421195856c045080d945cfa4f681
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2025-09-22 11:44:08 +0200

    instrukcja static ready

--- src/components/SceneInstruction.vue
+++ src/components/SceneInstruction.vue
@@ -7,22 +7,40 @@
         <div class="container">
             <div class="inner-box">
                 <h1 class="title">
-                    Kto wygra milion złotych? Zasady gry
+                    Kto wygra milion? Zasady gry
                 </h1>
                 <div class="kola-ratunkowe">
                     <span class="kola-elementy" id="kola-naglowek">Koła ratunkowe:</span>
-                    <div class="kola-elementy" >
+                    <div class="kola-elementy">
                         <div class="elipsa">
-                         50:50
+                            50:50
                         </div>
                         <p class="podpis-1">usuń dwie</p>
                         <p class="podpis-1">złe odpwiedzi</p>
                     </div>
-                    <div class="kola-elementy">usuń jedną</div>
-                    <div class="kola-elementy">wymien</div>
-                    <div class="kola-elementy">podpowiedz</div>
+                    <div class="kola-elementy">
+                        <div class="elipsa">
+                            75:100
+                        </div>
+                        <p class="podpis-1">usuń jedną</p>
+                        <p class="podpis-1">złą odpwiedź</p>
+                    </div>
+                    <div class="kola-elementy">
+                        <div class="elipsa elipsa-wymien">
+                            <img src="../assets/wymien.png" alt="wymien pytanie" width="60px" height="100px" />
+                        </div>
+                        <p class="podpis-1 podpis-wymien">wymień</p>
+                        <p class="podpis-1 podpis-pytanie">pytanie</p>
+                    </div>
+                    <div class="kola-elementy">
+                        <div class="elipsa elipsa-podpowiedz">
+                            <img src="../assets/podpowiedz.png" alt="wymien pytanie" width="90px" height="80px" />
+                        </div>
+                        <p class="podpis-1 podpis-odpowiedz">podpowiedź</p>
+                        <p class="podpis-1"></p>
+                    </div>
                 </div>
-                <ul class="zasady">
+                <ul class="zasady" tabindex="0">
                     <li>Gra polega na udzieleniu poprawnej odpowiedzi na 10 pytań, podzielonych na dwa poziomy
                         trudności: łatwy i trudny. Twoim
                         celem jest ukończenie gry i zdobycie złotego pucharu.</li>
@@ -44,9 +62,20 @@
             </div>
         </div>
     </div>
+    <!-- <div class="linia-test"></div> -->
 </template>
 
-<style>
+<style scoped>
+.linia-test {
+    width: 1900px;
+    height: 4px;
+    background-color: black;
+    position: absolute;
+    top: 340px;
+    right: 0px;
+    z-index: 2;
+}
+
 .tlo {
     background-image: url("../assets/latarnia.png");
     background-size: 1920px 1080px;
@@ -83,6 +112,7 @@
     font-style: bold;
     font-weight: 700;
     font-family: "Proxima Nova", sans-serif;
+    margin-top:-20px;
 }
 
 .kola-ratunkowe {
@@ -95,21 +125,27 @@
     align-items: center;
     justify-content: flex-start;
     align-self: flex-start;
-    gap: 24px;  
+    gap: 24px;
     position: relative;
+    margin-left: 181px;
+    margin-bottom: 40px;
 }
 
-.kola-elementy{
- display: inline-block;
- position: relative;
- text-align: center;
+li{
+    margin-bottom: 10px;
 }
 
-#kola-naglowek{
+.kola-elementy {
+    display: inline-block;
+    position: relative;
+    text-align: center;
+}
+
+#kola-naglowek {
     margin-top: -110px;
 }
 
-.elipsa{
+.elipsa {
     display: block;
     width: 158px;
     height: 118px;
@@ -119,22 +155,59 @@
     margin-left: 25px;
     padding-top: 36px;
     margin-bottom: -20px;
+
 }
 
-.podpis-1{
+.elipsa-wymien {
+    padding-top: 15px !important;
+    margin-bottom: -0px !important;
+}
+
+.elipsa-podpowiedz {
+    padding-top: 21px !important;
+    margin-bottom: -7px !important;
+   
+}
+
+.podpis-1 {
     margin-top: 0px;
     margin-bottom: 0px;
-}
-
-.zasady {
-    color: #093343;
-    font-size: 35px;
+    font-size: 30px;
     font-style: medium;
     font-weight: 500;
     font-family: "Proxima Nova", sans-serif;
 }
 
-.button-start{
+.podpis-odpowiedz {
+    margin-bottom: 42px !important;
+    margin-left: 25px;
+}
+
+.podpis-wymien {
+    margin-left: 25px;
+}
+
+.podpis-pytanie {
+    margin-left: 25px;
+}
+
+
+.zasady {
+    color: #093343;
+    font-size: 30px;
+    font-style: medium;
+    font-weight: 500;
+    font-family: "Proxima Nova", sans-serif;
+    margin-left: 56px;
+    margin-right: 50px;
+}
+
+.zasady:focus{
+      outline: 2px solid black;
+  outline-offset: 10px;
+}
+
+.button-start {
     color: white;
     background-color: #093343;
     width: 260px;
@@ -144,5 +217,10 @@
     font-style: bold;
     font-weight: 600;
     font-family: "Proxima Nova", sans-serif;
+}
+
+.button-start:focus{
+  outline: 5px solid black;
+  outline-offset: 10px;
 }
 </style>
